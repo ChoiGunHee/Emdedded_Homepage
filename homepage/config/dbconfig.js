e295516caca5e02894e05c5b15b15e34862e88c5
ChoiGunHee <godrjsmgl@gmail.com>
2018-06-24 23:08:18 +0900

    dbconfig 추가

---
+++ homepage/config/dbconfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host : '220.149.250.100',
+  user : 'root',
+  password : 'dstargram',
+  database : 'embedded_lab'
+}
--- homepage/routes/users.js
+++ homepage/routes/users.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+const connection = mysql.createConnection(require('../config/dbconfig.js'));
+
+connection.connect((err) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log( 'mysql connect completed' );
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  res.render('dashboard/users');
+  //res.render('dashboard/users');
+  const sql = 'select * from member';
+  connection.query(sql, (err, results, field) => {
+          console.log(results); // 배열 형태로 결과가 떨어짐
+          res.render('dashboard/users', {
+              //layout: false, // express-ejs-layouts는 기본으로 layout.ejs가 설정되어야 하는데 이를 사용하지 않을 경우
+              projects: results
+          });
+      });
 });
 
 module.exports = router;
