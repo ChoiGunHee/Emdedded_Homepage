4a78a26adc7f330c1f116be557be6c361bcd4709
ChoiGunHee <godrjsmgl@gmail.com>
2018-06-25 09:50:02 +0900

    병합

--- homepage/config/dbconfig.js
+++ homepage/config/dbconfig.js
@@ -1,13 +1,8 @@
 module.exports = {
   host : '220.149.250.100',
-<<<<<<< HEAD
   //host : 'localhost',
   user : 'root',
-=======
- // host : 'localhost',
-//  user : 'root',
   user : 'choigunhee',
->>>>>>> 82771136a77dee03c82584ec100ce75b09f5169d
   password : 'dstargram',
   database : 'embedded_lab'
 }
