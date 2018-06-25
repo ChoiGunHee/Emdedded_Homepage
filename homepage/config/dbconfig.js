82771136a77dee03c82584ec100ce75b09f5169d
ChoiGunHee <godrjsmgl@gmail.com>
2018-06-25 09:47:33 +0900

    test db 2

--- homepage/config/dbconfig.js
+++ homepage/config/dbconfig.js
@@ -1,7 +1,8 @@
 module.exports = {
- // host : '220.149.250.100',
-  host : 'localhost',
-  user : 'root',
+  host : '220.149.250.100',
+ // host : 'localhost',
+//  user : 'root',
+  user : 'choigunhee',
   password : 'dstargram',
   database : 'embedded_lab'
 }
