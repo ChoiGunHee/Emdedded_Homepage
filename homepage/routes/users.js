59c4297160a70567437960874ac300ffc24bb344
ChoiGunHee <godrjsmgl@gmail.com>
2018-06-25 09:48:42 +0900

    test db 3

--- homepage/routes/users.js
+++ homepage/routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/*
 var mysql = require('mysql');
+
 const connection = mysql.createConnection(require('../config/dbconfig.js'));
 
 connection.connect((err) => {
@@ -11,19 +13,20 @@ connection.connect((err) => {
     }
     console.log( 'mysql connect completed' );
 });
+*/
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  //res.render('dashboard/users');
-  const sql = 'select * from member';
+  res.render('dashboard/users');
+  /*const sql = 'select * from member';
   connection.query(sql, (err, results, field) => {
           console.log(results); // 배열 형태로 결과가 떨어짐
           res.render('dashboard/users', {
               //layout: false, // express-ejs-layouts는 기본으로 layout.ejs가 설정되어야 하는데 이를 사용하지 않을 경우
               projects: results
           });
-      });
+      });*/
 });
 
 module.exports = router;
